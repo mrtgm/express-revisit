fb197584cb5454e05d6d27f8939fec490a8ec1b3
Morio <xtarako@gmail.com>
2022-11-12 11:24:59 +0900

    seed

---
+++ MongoBlog/backend/seed.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import { ArticleModel } from "./models";
+
+mongoose.connect(process.env.MONGO_DB_URI as string);
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("Successfully connected to MongoDB using Mongoose!");
+});
+
+const seed = async () => {
+  await ArticleModel.deleteMany({});
+
+  const articles = [
+    {
+      title: "First Article",
+      author: "John Doe",
+      content: "This is the first article",
+      published: true,
+    },
+    {
+      title: "Second Article",
+      author: "John Doe",
+      content: "This is the second article",
+      published: true,
+    },
+    {
+      title: "Third Article",
+      author: "John Doe",
+      content: "This is the third article",
+      published: false,
+    },
+  ];
+
+  const res = await ArticleModel.insertMany(articles);
+
+  console.log("Database seeded", res);
+};
+
+seed().then(() => db.close());
